e7c3b52c8b802b08b4b0789372cf523b97016d00
Elena Radchenko <elena.radchenko.9@mail.ru>
2021-11-23 16:40:23 +0300

    added some changes to file 4

--- Elena_Radchenko_dz_3/lesson_3_dz_4.py
+++ Elena_Radchenko_dz_3/lesson_3_dz_4.py
@@ -9,4 +9,5 @@ def thesaurus_adv(*names_surnames):
         staff_dict[surname[0]][name[0]].append(names_surname)
     return staff_dict
 
+
 print(thesaurus_adv("Михаил Степанов", "Павел Назаров", "Елена Иванова"))
